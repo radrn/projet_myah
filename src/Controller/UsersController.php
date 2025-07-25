48e2e720f4917f2b6c1df325c82a3e6c4984c889
Radia Drine <drineradiaa@gmail.com>
2025-07-25 10:10:03 +0200

    partial added

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -13,26 +13,28 @@ final class UsersController extends AbstractController
 {
     #[Route('/profile/{username}', name: 'app_users')]
     public function show(
+        string             $username,
         UserRepository     $userRepository,
         PostRepository     $postRepository,
         PostLikeRepository $likeRepository,
-
     ): Response
     {
         $user = $this->getUser();
-        
-        $users = $userRepository->findBy(['username']);
+        $users = $userRepository->findOneBy(['username' => $username]);
 
         if (!$user) {
             throw $this->createNotFoundException("Utilisateur non trouvé.");
         }
+
         $posts = $postRepository->findBy(['user' => $users], ['createdAt' => 'DESC']);
         $likes = $likeRepository->findBy(['user' => $users], ['createdAt' => 'DESC']);
 
         return $this->render('users/users.html.twig', [
+            'user' => $user,
             'users' => $users,
             'posts' => $posts,
             'likes' => $likes,
         ]);
     }
+
 }
