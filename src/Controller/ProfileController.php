a26d1579c28ac78d053563a1adfbd72c611e6957
Radia Drine <drineradiaa@gmail.com>
2025-07-29 16:12:19 +0200

    comment section added

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,27 +2,34 @@
 
 namespace App\Controller;
 
-use App\Factory\CommentFactory;
 use App\Repository\CommentRepository;
 use App\Repository\PostLikeRepository;
 use App\Repository\PostRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
+
+    #[Route('/profile/{username?}', name: 'app_profile')]
     public function show(
+        ?string            $username,
+        UserRepository     $userRepository,
         PostRepository     $postRepository,
         PostLikeRepository $likeRepository,
         CommentRepository  $commentRepository
     ): Response
     {
         $user = $this->getUser();
+        if ($username === null && $user === null) {
+            $this->addFlash('warning', 'Utilisateur non trouvé');
+            return $this->redirectToRoute('app_login');
+        }
 
-        if (!$user) {
-            throw $this->createNotFoundException("Utilisateur non trouvé.");
+        if ($username !== null) {
+            $user = $userRepository->findOneBy(['username' => $username]);
         }
 
         $posts = $postRepository->findBy(['user' => $user], ['createdAt' => 'DESC']);
@@ -30,6 +37,7 @@ final class ProfileController extends AbstractController
         $comments = $commentRepository->findBy(['user' => $user], ['createdAt' => 'DESC']);
         $followersCount = $user->getSubscribeFolloweds()->count();
         $followingCount = $user->getSubscribes()->count();
+
         return $this->render('profile/profile.html.twig', [
             'user' => $user,
             'posts' => $posts,
